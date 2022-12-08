a937105b8c21b7c0cb92355dd4b80da33be247ae
Adimac93 <adimac93@gmail.com>
2022-12-08 23:58:54 +0100

    feat(backend): in message url parsing

--- frontend/src/lib/chat/Message.svelte
+++ frontend/src/lib/chat/Message.svelte
@@ -1,32 +1,58 @@
 <script lang="ts">
-    export let message: MessageModel;
-    let date = new Date(message.sat * 1000).toLocaleTimeString();
-    let sender = message.sender;
-    let content = message.content;
+  export let message: MessageModel;
+  const date = new Date(message.sat * 1000).toLocaleTimeString();
+  const sender = message.sender;
+
+  const parts: Array<Part> = [];
+
+  for (let word of message.content.split(" ")) {
+    const isUrl =
+      /(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/gm.test(
+        word
+      );
+    parts.push({ isUrl, payload: word + " " });
+  }
+
+  interface Part {
+    payload: string;
+    isUrl: boolean;
+  }
 </script>
 
 <div class="message">
-    <strong class="sender">{sender}</strong>
-    <i class="date">{date}</i>
-    <div class="content">{content}</div>
+  <strong class="sender">{sender}</strong>
+  <i class="date">{date}</i>
+  <div class="content">
+    {#each parts as part}
+      {#if part.isUrl}
+        <a class="part" href={part.payload}>{part.payload}</a>
+      {:else}
+        <div class="part">{part.payload}</div>
+      {/if}
+    {/each}
+  </div>
 </div>
 
 <style>
-    .message {
-        text-align: left;
-    }
-    .message:hover {
-        filter: brightness(85%);
-    }
-    .sender {
-        display: inline;
-        text-align: left;
-    }
-    .date {
-        display: inline;
-        text-align: right;
-    }
-    .content {
-        text-align: left;
-    }
+  .message {
+    text-align: left;
+  }
+  .message:hover {
+    filter: brightness(85%);
+  }
+  .sender {
+    display: inline;
+    text-align: left;
+  }
+  .date {
+    display: inline;
+    text-align: right;
+  }
+  .content {
+    text-align: left;
+  }
+
+  .part {
+    display: inline;
+  }
 </style>
