a73521032d6872b474056501614399db38ec7ab0
Adimac93 <adimac93@gmail.com>
2023-01-14 22:09:04 +0100

    ✏️ gget

--- backend/tests/chat.rs
+++ backend/tests/chat.rs
@@ -1,4 +1,4 @@
-﻿use backend::utils::chat::{create_message, errors::ChatError, gget_user_email_by_id;
+﻿use backend::utils::chat::{create_message, errors::ChatError, get_user_email_by_id};
 use sqlx::PgPool;
 use uuid::Uuid;
 
