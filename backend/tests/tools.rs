4084ed59bc5da2833d178451024e2736a7d93d7a
Adimac93 <adimac93@gmail.com>
2023-01-18 22:40:15 +0100

    ⚰️ remove test tracing subscriber
    ~ tracing problem resolved with dep `tracing_test`

--- backend/tests/tools.rs
+++ backend/tests/tools.rs
@@ -3,22 +3,12 @@ use dotenv::dotenv;
 use reqwest::Client;
 use sqlx::PgPool;
 use std::net::{SocketAddr, TcpListener};
-use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
 
 async fn spawn_app(db: PgPool) -> SocketAddr {
     dotenv().ok();
 
     let listener = TcpListener::bind(SocketAddr::from(([127, 0, 0, 1], 0))).unwrap();
     let addr = listener.local_addr().unwrap();
-
-    // !panics when set for the second time - can't run multiple tests setting tracing subscriber
-
-    // tracing_subscriber::registry()
-    //     .with(tracing_subscriber::EnvFilter::new(
-    //         std::env::var("RUST_LOG").unwrap_or_else(|_| "backend=debug".into()),
-    //     ))
-    //     .with(tracing_subscriber::fmt::layer())
-    //     .init();
 
     let settings = get_config().unwrap();
     tokio::spawn(async move {
