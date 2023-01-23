7819118a8e92071cf6e509419b2bd204f6488adc
Adimac93 <adimac93@gmail.com>
2023-01-23 17:28:15 +0100

    🗃️ networks & user agents

---
+++ backend/migrations/20230116115214_user_login_ip.down.sql
@@ -0,0 +1,6 @@
+-- Add down migration script here
+
+drop table user_network_browser_agents;
+drop table user_networks;
+drop table browser_agents;
+drop table networks;
---
+++ backend/migrations/20230116115214_user_login_ip.up.sql
@@ -0,0 +1,32 @@
+-- Add up migration script here
+
+create table networks (
+    ip inet not null primary key,
+    geolocation_data jsonb not null
+
+);
+
+-- unique browser agent
+create table browser_agents (
+    id uuid not null default gen_random_uuid() primary key,
+    user_agent_data jsonb not null unique
+);
+
+create table user_networks (
+    primary key (user_id, network_ip),
+    network_ip inet not null,
+    user_id uuid not null,
+    foreign key (network_ip) references networks(ip),
+    foreign key (user_id) references users(id)
+);
+-- browser agents used from the same ip and user
+create table user_network_browser_agents (
+    primary key (user_id, network_ip, browser_agent_id),
+    is_trusted boolean not null,
+    network_ip inet not null,
+    browser_agent_id uuid not null,
+    user_id uuid not null,
+    foreign key (network_ip) references networks(ip),
+    foreign key (browser_agent_id) references browser_agents(id),
+    foreign key (user_id) references users(id)
+);
