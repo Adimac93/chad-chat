c5a0663ea0a627b5e231625dbe9c301cc9634314
HubertK05 <hubert.klimowicz.hk@gmail.com>
2023-10-14 19:33:05 +0200

    :construction: upgrade error handling

--- backend/src/errors.rs
+++ backend/src/errors.rs
@@ -1,29 +1,23 @@
-use crate::utils::friends::errors::FriendError;
-use crate::utils::{
-    auth::errors::AuthError, chat::errors::ChatError, groups::errors::GroupError,
-    invitations::errors::InvitationError, roles::errors::RoleError,
-};
+use anyhow::anyhow;
 use axum::http::StatusCode;
-use axum::response::IntoResponse;
+use axum::response::{IntoResponse, Response};
 use axum::Json;
 use serde::Serialize;
+use sqlx::error::{DatabaseError, ErrorKind};
+use sqlx::{query, Error, PgPool};
 use thiserror::Error;
-use tracing::error;
+use tracing::{debug, error};
+use typeshare::typeshare;
 
 #[derive(Error, Debug)]
 pub enum AppError {
+    #[error("{code} - {message}")]
+    Expected { code: StatusCode, message: String },
     #[error(transparent)]
-    AuthError(#[from] AuthError),
-    #[error(transparent)]
-    GroupError(#[from] GroupError),
-    #[error(transparent)]
-    ChatError(#[from] ChatError),
-    #[error(transparent)]
-    FriendError(#[from] FriendError),
-    #[error(transparent)]
-    Unexpected(anyhow::Error),
+    Unexpected(#[from] anyhow::Error),
 }
 
+#[typeshare]
 #[derive(Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct ErrorResponse {
@@ -36,42 +30,93 @@ impl ErrorResponse {
     }
 }
 
-// TODO: server error backtrace
 impl IntoResponse for AppError {
-    fn into_response(self) -> axum::response::Response {
-        match self {
-            AppError::AuthError(e) => return e.into_response(),
-            AppError::GroupError(e) => return e.into_response(),
-            AppError::ChatError(e) => return e.into_response(),
-            AppError::FriendError(e) => return e.into_response(),
+    fn into_response(self) -> Response {
+        let error_message = self.to_string();
+        let (code, message) = match self {
+            AppError::Expected { code, message } => {
+                debug!("{error_message}");
+                (code, ErrorResponse::json(message))
+            }
             AppError::Unexpected(_) => {
-                let error_message = self.to_string();
                 error!("{error_message}");
                 (
                     StatusCode::INTERNAL_SERVER_ERROR,
                     ErrorResponse::json("Unexpected server error".into()),
                 )
-                    .into_response()
             }
+        };
+        (code, message).into_response()
+    }
+}
+
+impl AppError {
+    pub fn exp(code: StatusCode, message: &str) -> Self {
+        Self::Expected {
+            code,
+            message: message.to_string(),
         }
     }
 }
 
-// better error conversion trait may be needed
-impl From<InvitationError> for AppError {
-    fn from(e: InvitationError) -> Self {
-        Self::from(GroupError::from(e))
-    }
-}
-
-impl From<RoleError> for AppError {
-    fn from(e: RoleError) -> Self {
-        Self::from(GroupError::from(e))
-    }
-}
-
 impl From<sqlx::Error> for AppError {
-    fn from(e: sqlx::Error) -> Self {
-        Self::Unexpected(anyhow::anyhow!(e))
+    fn from(val: Error) -> Self {
+        Self::Unexpected(anyhow!(val))
+    }
+}
+
+pub struct DbErrMessage {
+    pub err: AppError,
+    pub kind: ErrorKind,
+    pub name: String,
+}
+
+impl DbErrMessage {
+    pub fn new(err: sqlx::Error) -> Self {
+        let (kind, name) = if let Some(e) = err.as_database_error() {
+            (e.kind(), e.constraint().unwrap_or_default().to_string())
+        } else {
+            (ErrorKind::Other, "".to_string())
+        };
+
+        Self {
+            err: AppError::from(err),
+            kind,
+            name,
+        }
+    }
+
+    pub fn fk(mut self, code: StatusCode, message: &str) -> Self {
+        if self.kind == ErrorKind::ForeignKeyViolation {
+            self.err = AppError::exp(code, message);
+        }
+        self
+    }
+
+    pub fn unique(mut self, code: StatusCode, message: &str) -> Self {
+        if self.kind == ErrorKind::UniqueViolation {
+            self.err = AppError::exp(code, message);
+        }
+        self
+    }
+
+    pub fn not_null(mut self, code: StatusCode, message: &str) -> Self {
+        if self.kind == ErrorKind::NotNullViolation {
+            self.err = AppError::exp(code, message);
+        }
+        self
+    }
+
+    pub fn check(mut self, code: StatusCode, message: &str) -> Self {
+        if self.kind == ErrorKind::CheckViolation {
+            self.err = AppError::exp(code, message);
+        }
+        self
+    }
+}
+
+impl From<DbErrMessage> for AppError {
+    fn from(val: DbErrMessage) -> Self {
+        val.err
     }
 }
