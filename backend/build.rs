03cf4c3edd41f05709fc3ec55bc1d34a1e06d4ee
Adimac93 <adimac93@gmail.com>
2023-10-27 20:03:32 +0200

    install vite for production

--- backend/build.rs
+++ backend/build.rs
@@ -1,6 +1,16 @@
-use std::process::Command;
+use std::{env::var, process::Command};
 
 fn main() {
+    if let Some(true) = var("APP_ENVIRONMENT").ok().map(|v| &v == "production") {
+        let status = Command::new("npm")
+            .arg("install")
+            .arg("vite")
+            .current_dir("../frontend")
+            .status()
+            .expect("failed to install Vite");
+
+        assert!(status.success())
+    }
     let status = Command::new("npm")
         .arg("run")
         .arg("build")
