d691394afbd5fc3fca13ab40ce85da83a800bb30
Adimac93 <adimac93@gmail.com>
2023-11-24 21:47:25 +0100

    🧑‍💻 fix Vite dev server CORS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,33 +1,41 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
-// https://stackoverflow.com/questions/71534594/change-vite-proxy-location-automatically-in-dev-vs-prod-builds
-export default defineConfig({
-    plugins: [svelte()],
-    server: {
-        proxy: {
-            "/api": {
-                target: "http://127.0.0.1:3000",
-                changeOrigin: true,
-                secure: false,
-                ws: true,
-                configure: (proxy, _options) => {
-                    proxy.on("error", (err, _req, _res) => {
-                        console.log("proxy error", err);
-                    });
-                    proxy.on("proxyReq", (proxyReq, req, _res) => {
-                        console.log("Sending Request to the Target:", req.method, req.url);
-                    });
-                    proxy.on("proxyRes", (proxyRes, req, _res) => {
-                        console.log(
-                            "Received Response from the Target:",
-                            proxyRes.statusCode,
-                            req.url
-                        );
-                    });
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    const backendURL = JSON.stringify(env.BACKEND_URL) ?? "http://127.0.0.1:3000"
+    console.info(`Backend at ${backendURL}`)
+    return {
+        plugins: [svelte()],
+        server: {
+            // https://github.com/expressjs/cors#configuration-options
+            cors: {
+                origin: backendURL,
+            },
+            proxy: {
+                "/api": {
+                    target: backendURL,
+                    changeOrigin: true,
+                    secure: false,
+                    ws: true,
+                    configure: (proxy, _options) => {
+                        proxy.on("error", (err, _req, _res) => {
+                            console.log("proxy error", err);
+                        });
+                        proxy.on("proxyReq", (proxyReq, req, _res) => {
+                            console.log("Sending Request to the Target:", req.method, req.url);
+                        });
+                        proxy.on("proxyRes", (proxyRes, req, _res) => {
+                            console.log(
+                                "Received Response from the Target:",
+                                proxyRes.statusCode,
+                                req.url
+                            );
+                        });
+                    },
                 },
             },
-        },
-    },
+        }
+    }
 });
