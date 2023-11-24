3301b87687de802c2abdd28b43a646f3e3c14130
Adimac93 <adimac93@gmail.com>
2023-11-24 15:59:03 +0100

    ✨ graceful shutdown

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,6 +8,7 @@ pub mod utils;
 use crate::modules::extractors::addr::ClientAddr;
 use crate::{configuration::get_config, routes::app};
 use dotenv::dotenv;
+use tokio::signal;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use tracing_subscriber::{EnvFilter, Layer};
 
@@ -39,6 +40,33 @@ async fn main() {
                 .await
                 .into_make_service_with_connect_info::<ClientAddr>(),
         )
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .expect("Failed to run axum server");
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("ya soon");
 }
