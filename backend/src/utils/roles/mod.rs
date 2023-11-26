d7ed3650d70c2fe1c95cc6634417f5233940b70d
HubertK05 <hubert.klimowicz.hk@gmail.com>
2023-11-26 08:56:25 +0100

    :construction: remove role queries

--- backend/src/utils/roles/mod.rs
+++ backend/src/utils/roles/mod.rs
@@ -1,108 +1,48 @@
 ﻿pub mod models;
 pub mod privileges;
 
-use sqlx::{query, Acquire, PgPool, Postgres};
+use sqlx::{Acquire, PgPool, Postgres};
 use uuid::Uuid;
 
-use crate::errors::AppError;
-use self::{
-    models::{
-        GroupRolePrivileges, PrivilegeChangeData, PrivilegeInterpretationData, Role,
-        UserRoleChangeData,
-    },
-    privileges::{Privilege, Privileges, QueryPrivilege},
-};
-
-pub async fn single_set_group_role_privileges<'c>(
-    conn: impl Acquire<'c, Database = Postgres> + std::marker::Send,
-    data: &PrivilegeChangeData,
-) -> Result<(), AppError> {
-    match data.value {
-        Privilege::CanInvite(x) => x.set_privilege(conn, data).await?,
-        Privilege::CanSendMessages(x) => x.set_privilege(conn, data).await?,
+use crate::{errors::AppError, state::RdPool};
+use self::models::{
+        PrivilegeChangeInput, Role,
+        UserRoleChangeInput, GroupPrivileges, UserPrivileges,
     };
 
-    Ok(())
-}
-
-pub async fn get_group_role_privileges(
-    pool: &PgPool,
-    group_id: Uuid,
-) -> Result<GroupRolePrivileges, AppError> {
-    let query_res = query!(
-        r#"
-            SELECT group_roles.role_type as "role_type: Role", roles.can_invite, roles.can_send_messages from
-                group_roles JOIN roles ON group_roles.role_id = roles.id
-                WHERE group_roles.group_id = $1
-                AND group_roles.role_type IN ('member', 'admin')
-                ORDER BY group_roles.role_type
-        "#,
-        group_id
-    )
-    .fetch_all(pool)
-    .await?;
-
-    let mut res = GroupRolePrivileges::new();
-    for role_data in query_res {
-        res.0.insert(
-            role_data.role_type,
-            Privileges::try_from(PrivilegeInterpretationData {
-                can_invite: role_data.can_invite,
-                can_send_messages: role_data.can_send_messages,
-            })?,
-        );
-    }
-
-    Ok(res)
-}
-
-pub async fn single_set_group_user_role<'c>(
-    conn: impl Acquire<'c, Database = Postgres>,
-    data: &UserRoleChangeData,
+pub async fn set_privileges<'c>(
+    conn: &RdPool,
+    data: &PrivilegeChangeInput,
 ) -> Result<(), AppError> {
-    let mut transaction = conn.begin().await?;
+    todo!()
+}
 
-    let _res = query!(
-        r#"
-            UPDATE group_users
-            SET role_id = group_roles.role_id
-            FROM group_roles
-            WHERE group_roles.group_id = $1
-            AND group_users.user_id = $2
-            AND group_roles.role_type = $3
-        "#,
-        data.group_id,
-        data.user_id,
-        data.value as Role,
-    )
-    .execute(&mut *transaction)
-    .await?;
+pub async fn get_all_privileges(
+    pool: &RdPool,
+    group_id: Uuid,
+) -> Result<GroupPrivileges, AppError> {
+    todo!()
+}
 
-    transaction.commit().await?;
-
-    Ok(())
+pub async fn set_role<'c>(
+    conn: &RdPool,
+    data: &UserRoleChangeInput,
+) -> Result<(), AppError> {
+    todo!()
 }
 
 pub async fn get_user_role(
-    pool: &PgPool,
+    pool: &RdPool,
     user_id: &Uuid,
     group_id: &Uuid,
 ) -> Result<Role, AppError> {
-    let res = query!(
-        r#"
-            SELECT group_roles.role_type AS "role: Role"
-            FROM group_users 
-            JOIN roles 
-            JOIN group_roles ON roles.id = group_roles.role_id
-            ON group_users.role_id = roles.id
-            WHERE group_users.user_id = $1
-            AND group_users.group_id = $2
-        "#,
-        user_id,
-        group_id,
-    )
-    .fetch_one(pool)
-    .await?;
+    todo!()
+}
 
-    Ok(res.role)
+pub async fn get_user_privileges(
+    pool: &RdPool,
+    user_id: Uuid,
+    group_id: Uuid,
+) -> Result<UserPrivileges, AppError> {
+    todo!()
 }
